2c39647c5c3fce6983b05c637f0995c8695e9c2e
Joan Roberto Garcia Garcia <joangarcia2015@gmail.com>
2021-10-06 02:30:22 -0500

    JS and CSS compressed

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const { src, dest, watch, parallel } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const webp = require("gulp-webp");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
+const concat = require("gulp-concat");
+const terser = require("gulp-terser-js");
+const rename = require("gulp-rename");
 
 const paths = {
     scss: "src/scss/**/*.scss",
@@ -9,13 +16,25 @@ const paths = {
 };
 
 function css() {
-    return src(paths.scss)
-        .pipe(
-            sass({
-                outputstyle: "expanded",
-            })
-        )
-        .pipe(dest("./build/css"));
+    return (
+        src(paths.scss)
+            .pipe(sourcemaps.init())
+            .pipe(sass())
+            .pipe(postcss([autoprefixer(), cssnano()]))
+            // .pipe(postcss([autoprefixer()]))
+            .pipe(sourcemaps.write("."))
+            .pipe(dest("./build/css"))
+    );
+}
+
+function javascript() {
+    return src(paths.js)
+        .pipe(sourcemaps.init())
+        .pipe(concat("bundle.js")) // final output file name
+        .pipe(terser())
+        .pipe(sourcemaps.write("."))
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(dest("./build/js"));
 }
 
 function versionWebp() {
@@ -24,7 +43,8 @@ function versionWebp() {
 
 function watchArchivos() {
     watch(paths.scss, css);
+    watch(paths.js, javascript);
 }
 
 exports.versionWebp = versionWebp;
-exports.default = parallel(css, watchArchivos);
+exports.default = parallel(css, javascript, watchArchivos);
